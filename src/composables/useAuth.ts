5146f3ba6eed0ad664327a8c2b8c5f482fe48eba
Rueben <Rueben.cumberbatch@gamma.co.uk>
2025-05-21 17:01:01 +0100

    feat: create authentication composable

---
+++ src/composables/useAuth.ts
@@ -0,0 +1,82 @@
+import { ref, type Ref } from "vue";
+import axios, { AxiosError } from "axios";
+
+const API_URL = "http:/localhost:3000/auth";
+
+interface UserCredentials {
+  email: string;
+  password: string;
+}
+
+interface AuthenticationResponse {
+  access_token: string;
+}
+
+interface AuthenticationErrorResponse {
+  message: string;
+}
+
+interface UseAuthComposable {
+  isLoading: Ref<boolean>;
+  error: Ref<string | null>;
+  handleLogin: (userCredentials: UserCredentials) => Promise<void>;
+  handleRegistration: (userCredentials: UserCredentials) => Promise<void>;
+}
+
+export const useAuth = (): UseAuthComposable => {
+  const isLoading = ref<boolean>(false);
+  const error = ref<string | null>(null);
+
+  const handleLogin = async (
+    userCredentials: UserCredentials
+  ): Promise<void> => {
+    isLoading.value = true;
+    error.value = null;
+
+    try {
+      const response = await axios.post<AuthenticationResponse>(
+        `${API_URL}/login`,
+        userCredentials
+      );
+      const token = response.data.access_token;
+
+      localStorage.setItem("access-token", token);
+    } catch (err) {
+      const errorResponse = err as AxiosError<AuthenticationErrorResponse>;
+      error.value =
+        errorResponse?.response?.data?.message || "Login Attempt Failed";
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  const handleRegistration = async (
+    userCredentials: UserCredentials
+  ): Promise<void> => {
+    isLoading.value = true;
+    error.value = null;
+
+    try {
+      const response = await axios.post<AuthenticationResponse>(
+        `${API_URL}/register`,
+        userCredentials
+      );
+      const token = response.data.access_token;
+
+      localStorage.setItem("access-token", token);
+    } catch (err) {
+      const errorResponse = err as AxiosError<AuthenticationErrorResponse>;
+      error.value =
+        errorResponse?.response?.data?.message || "Registration Attempt Failed";
+    } finally {
+      isLoading.value = false;
+    }
+  };
+
+  return {
+    isLoading,
+    error,
+    handleLogin,
+    handleRegistration,
+  };
+};
